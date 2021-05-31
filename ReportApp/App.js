0f87bc07aef39b8ac2824e37930c77ed420c2bbb
dilroop123 <diroop.singh.ds@gmail.com>
2021-05-31 02:18:21 -0400

    Added packages

--- ReportApp/App.js
+++ ReportApp/App.js
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {
   SafeAreaView,
   ScrollView,
@@ -17,37 +17,21 @@ import {
 
 
 const App= () => {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    <SafeAreaView>
+      <StatusBar  />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        >
+        
+        <View>
+        
+             <Text style={styles.highlight}>This is main loading screen..boom</Text> 
+     
+       
         </View>
       </ScrollView>
     </SafeAreaView>
